5105665aa89154d97315603917fa07645dbbbbcb
Renks <srrenks@gmail.com>
2022-06-19 22:28:04 -0300

    feat: reads all pdf files and generates arguments to get_data function

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from multiprocessing import Pool, freeze_support
 from app.get_data import get_data
 from app.read_xml import read_xml
+from app.read_pdf import read_pdf
 import os
 
+
+# multiprocessing for read_pdf
+def multi_read_pdf(pdf_path):
+    with Pool() as p:
+        return p.map(read_pdf, pdf_path)
+
+
+# multiprocessing for get_data
 def multi_get_data(data_list):
     with Pool() as p:
         p.starmap(get_data, data_list)
 
 
+# multiprocessing for read_xml
 def multi_read_xml(path_list):
     with Pool() as p:
         return p.map(read_xml, path_list)
 
+
 if __name__ == '__main__':
+    # if run in Windows, freeze_support is available
     if os.name == 'nt':
         freeze_support()
-    data_list = [(35220564748494000170550010000026011909709830, 'nfe')]
+    # path for pdf files
+    pdf_list = []
+    pdf_list.extend([os.path.join(".", file) for file in os.listdir(".") if file.endswith(".pdf")])
+    # arguments for get_data
+    data_list = multi_read_pdf(pdf_list)
     multi_get_data(data_list)
-    print(multi_read_xml(next(os.walk('tmp'), (None, None, []))[2]))
+    # get data for all xml files
+    xml_data = multi_read_xml(next(os.walk('tmp'), (None, None, []))[2])
