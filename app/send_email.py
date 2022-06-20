dfa74032fae0cd54d66c3eafd5ec849e1c04e8e6
Renks <srrenks@gmail.com>
2022-06-20 16:59:27 -0300

    feat: send error log to recipient emails

--- app/send_email.py
+++ app/send_email.py
@@ -5,21 +5,21 @@ import smtplib
 
 
 # your support email address
-email = 'youremail@example.com'
+email = 'example@example.com'
 # your recipient emails
-to = []
+to = ['exampledest1@example.com', 'exampledest2@example.com']
 # server and server port.
-server = 'yourserver.example.com'
-port = 25 # example port
+server = 'example.server.com'
+port = 25  # example port
 
 
 # send email notification when expiration date is coming soon
 def expiration_coming(pem, remaining_days, expiration_date):
-    # conect to server
+    # connect to server
     s = smtplib.SMTP(server, port)
     s.connect(server, port)
     s.ehlo()
-    # edit strings to send message to emails 
+    # edit strings to send message to emails
     pem = pem.replace('certificates/', '').replace('.pem', '.pfx')
     body = f'O seguinte certificado:\n"{pem}"\nVai expirar em {remaining_days} dias ({expiration_date}).\nContate o desenvolvedor o mais breve possível.\n'
     # send email for every email in recipient email list
@@ -29,6 +29,24 @@ def expiration_coming(pem, remaining_days, expiration_date):
             msg.attach(MIMEText(body))
             msg['To'] = dest
             msg['Subject'] = 'ALERTA: Certificado digital próximo da data de expiração'
+            msg['From'] = email
+            s.sendmail(email, [dest], msg.as_string())
+        except Exception as ex:
+            print("Something went wrong….", ex)
+
+def error_status(message):
+    # connect to server
+    s = smtplib.SMTP(server, port)
+    s.connect(server, port)
+    s.ehlo()
+    body = f'Log de erro:\n\n{message}\n\nContate o desenvolvedor o mais breve possível.'
+    # send email for every email in recipient email list
+    for dest in to:
+        try:
+            msg = MIMEMultipart()
+            msg.attach(MIMEText(body))
+            msg['To'] = dest
+            msg['Subject'] = 'ALERTA: erros durante a execução'
             msg['From'] = email
             s.sendmail(email, [dest], msg.as_string())
         except Exception as ex:
