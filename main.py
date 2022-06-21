a1c23b70a5ed570df2848dca0d7e6660d47ce9b0
Renks <srrenks@gmail.com>
2022-06-21 02:04:50 -0300

    feat: raise error when there is 'ERROR' on some item in 'status', sending an email to recipients.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from multiprocessing import Pool, freeze_support
+from app.send_email import error_status
 from app.get_data import get_data
 from app.read_xml import read_xml
 from app.read_pdf import read_pdf
@@ -14,7 +15,7 @@ def multi_read_pdf(pdf_path):
 # multiprocessing for get_data
 def multi_get_data(data_list):
     with Pool() as p:
-        p.starmap(get_data, data_list)
+        return p.starmap(get_data, data_list)
 
 
 # multiprocessing for read_xml
@@ -32,6 +33,22 @@ if __name__ == '__main__':
     pdf_list.extend([os.path.join(".", file) for file in os.listdir(".") if file.endswith(".pdf")])
     # arguments for get_data
     data_list = multi_read_pdf(pdf_list)
-    multi_get_data(data_list)
+    status = multi_get_data(data_list)
     # get data for all xml files
     xml_data = multi_read_xml(next(os.walk('tmp'), (None, None, []))[2])
+    # check if have a unespected error
+    ok = []
+    error = []
+    for item in status:
+        if 'ERROR' in item:
+            error.append(item)
+        elif 'OK' in item:
+            ok.append(item)
+
+    if error:
+        message = ''
+        for item in error:
+            message += f'{item}\n\n'
+        error_status(message)
+        raise Exception(message)
+    i
